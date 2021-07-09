64a79d9a34e88745abb58b67fbedcf57268de900
penanatalia56@gmail.com <penanatalia56@gmail.com>
2021-07-09 01:50:10 -0500

    plun administrador

---
+++ wp-content/plugins/administrador/administrador.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: administrador
+Plugin URI: https://localhost/lavamania/administrador
+Description: Plugin administrador
+Version: 1.0
+Author: Natalia
+Author URI: https://localhost/lavamania/administrador
+License: GPL2
+*/
+
+//administrador// Include mfp-opciones.php, use require_once to stop the script if mfp-functions.php is not found
+require_once plugin_dir_path(__FILE__) . 'includes/a-opciones.php';
+?>
